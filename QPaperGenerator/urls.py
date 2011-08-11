76d73497888b5c1760a0901ba9dfbc44adec338d
Gautam Kumar <gautham5678@gmail.com>
2011-08-11 07:28:35 +0530

    Basic question Paper Generation done

--- QPaperGenerator/urls.py
+++ QPaperGenerator/urls.py
@@ -5,13 +5,16 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^QuestionPaperFrom/', 'QPaperGenerator.QP.views.GenerateQuestionPaperGetDetails')
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+    
+    (r'^QuestionPaperFrom/', 'QPaperGenerator.QP.views.GenerateQuestionPaperGetDetails'),
+    (r'^GenerateQPaper/','QPaperGenerator.QP.views.GenerateQuestionPaperFromDetails'),
     # Example:
     # (r'^QPaperGenerator/', include('QPaperGenerator.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+
 )
